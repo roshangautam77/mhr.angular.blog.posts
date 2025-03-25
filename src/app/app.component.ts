72c68c1e2a06c8b511d9e9232b4d7fb3bb135925
roshangautam77 <roshangautam77@gmail.com>
2025-03-25 22:32:14 +0000

    app component changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { PostsComponent } from './components/posts/posts.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  standalone: true
+  standalone: true,
+  imports: [PostsComponent]
 })
 export class AppComponent {
   title = 'angular-take-home-test';
