b32ad18a5bfbb980d3340127bfb4cd4478705aeb
roshangautam77 <roshangautam77@gmail.com>
2025-03-25 22:30:27 +0000

    Modified interface and implemented posts endpoint in service

--- src/app/services/posts/posts.service.ts
+++ src/app/services/posts/posts.service.ts
@@ -3,11 +3,12 @@ import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IPost } from './interfaces/post.interface';
 
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class PostsService {
   private readonly _http: HttpClient = inject(HttpClient);
+  private readonly BASE_URL = 'http://localhost:3000';
 
   public getPosts(): Observable<Array<IPost>> {
-    return this._http.get<Array<IPost>>('http://localhost:3000/posts');
+    return this._http.get<Array<IPost>>(`${this.BASE_URL}/posts`);
   }
 }
